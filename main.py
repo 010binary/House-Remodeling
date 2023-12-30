07c865f6e54bd012218a77ee2fee442824d53537
010binary <augustine32fg@gmail.com>
2023-12-31 00:54:19 +0100

    routing to page 2 and manipulation with the jinja template

--- main.py
+++ main.py
@@ -2,10 +2,23 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
+# This is to handle any routing error - esp. 404
+@app.errorhandler(404)
+def error(error):
+    return render_template("error.html"), 404
+
+
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('index.html')
+    return render_template("index.html")
 
-if __name__ == '__main__':
+
+@app.route("/about-us")
+def about_us():
+    return render_template("about-us.html")
+
+
+if __name__ == "__main__":
     app.run(debug=True)
---
+++ static/javascript/carosel.js
@@ -0,0 +1,14 @@
+let slideNumber = 0;
+showSlides();
+
+function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideNumber++;
+    if (slideNumber > slides.length) { slideNumber = 1 }
+    slides[slideNumber - 1].style.display = "block";
+    setTimeout(showSlides, 2000); // Changes image every 2 seconds
+}
